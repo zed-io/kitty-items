f85ffb5b7a32ce821a386517b564d898a5a2b69e
jtaaa <joshuatallum@gmail.com>
2021-03-21 05:03:59 -0400

    Add link to diaper page to root page

--- web/src/pages/root.page.js
+++ web/src/pages/root.page.js
@@ -1,10 +1,17 @@
 import {Base} from "../parts/base.comp"
 import PageHeader from "../parts/page-header.comp"
+import {Link as RouterLink} from "react-router-dom"
+import {Center, Link, Text} from "@chakra-ui/layout"
 
 export function Page() {
   return (
     <Base>
       <PageHeader />
+      <Center height="500px">
+        <Link to="/diaper" as={RouterLink}>
+          <Text fontSize="3xl">Machel Montano's Diaper</Text>
+        </Link>
+      </Center>
     </Base>
   )
 }
